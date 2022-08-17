088eba1a8e11d563eabfd8fd92f33e1fe5fae6a7
Layanne Moraes <104206895+Anne-Moraes@users.noreply.github.com>
2022-08-17 13:00:17 -0300

    Atualização

---
+++ 1035 Bee Crowd/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1035_Bee_Crowd
+{
+    internal class Selecao
+    {
+        static void Main(string[] args)
+        {
+            testeDeSelecao();
+
+            /* Leia 4 valores inteiros A, B, C e D. Então se B for maior que C e D for maior que A e se a 
+            soma de C e D for maior que a soma de A e B e se C e D forem valores positivos e se A for par, 
+            escreva a mensagem “Valores aceitos” . Caso contrário, escreva a mensagem “Valores não aceitos”.  
+            
+            b > c && d > a && somaCD > somaAB && c > 0 && d > 0 && resto == 0
+
+            Entrada
+            Quatro números inteiros A, B, C e D.
+
+            Resultado
+            Mostre a mensagem correspondente após a validação dos valores​​. */
+        }
+        static void testeDeSelecao()
+        {
+            int a, b, c, d, somaCD, somaAB, resto;
+
+            string[] vet = Console.ReadLine().Split(' ');
+            a = int.Parse(vet[0]);
+            b = int.Parse(vet[1]);  
+            c = int.Parse(vet[2]);
+            d = int.Parse(vet[3]);
+
+            somaAB = a + b;
+            somaCD = c + d;
+
+            resto = a % 2;
+
+            if (b > c && d > a && somaCD > somaAB && c > 0 && d > 0 && resto == 0)
+            {
+                Console.WriteLine("Valores aceitos");
+            }
+            else
+            {
+                Console.WriteLine("Valores não aceitos");
+            }
+        }
+    }
+}
---
+++ 1038 Bee Crowd/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace _1038_Bee_Crowd
+{
+    internal class Snack
+    {
+        static void Main(string[] args)
+        {
+            snackProgram();
+
+            /* Using the following table, write a program that reads a code and the amount of an item. 
+            After, print the value to pay. This is a very simple program with the only intention of practice of 
+            selection commands.
+
+            Input
+            The input file contains two integer numbers X and Y. X is the product code and Y is the quantity of this 
+            item according to the above table.
+
+            Output
+            The output must be a message "Total: R$ " followed by the total value to be paid, with 2 digits after the 
+            decimal point.*/
+        }
+        static void snackProgram()
+        {
+            int x, y;
+            double price;
+
+            string[] vet = Console.ReadLine().Split(' ');
+
+            x = int.Parse(vet[0]);
+            y = int.Parse(vet[1]);
+
+            price = 0;
+
+            if (x == 1)
+            {
+                price = y * 4;
+            }
+            else if (x == 2)
+            {
+                price = y * 4.5;
+            }
+            else if (x == 3)
+            {
+                price = y * 5;
+            }
+            else if (x == 4)
+            {
+                price = y * 2;
+            }
+            else if (x == 5)
+            {
+                price = y * 1.5;
+            }
+            Console.WriteLine($"Total: R$ {price.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
